19b023299f55ff43cca804794436355455433a5e
somnathsingh712 <somnathsingh9517@gmail.com>
2026-09-09 02:53:40 +0530

    update

--- day2task1.py
+++ day2task1.py
@@ -1,38 +1,39 @@
 import sys
-from PyQt6.QtWidgets import *
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 
-class CounterApp(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.count=0
-        self.setWindowTitle("Simple Calculator")
-        self.resize(300,200)
+app = QApplication(sys.argv)
 
-        self.label=QLabel(str(self.count))
+window = QWidget()
+window.setWindowTitle("Counter App")
+window.resize(250, 200)
 
-        self.inc_btn=QPushButton("Increment")
-        self.dec_btn=QPushButton("Decrement")
+count = 0
 
-        self.inc_btn.clicked.connect(self.increment)
-        self.dec_btn.clicked.connect(self.decrement)
-        hbox=QHBoxLayout()
-        hbox.addWidget(self.inc_btn)
-        hbox.addWidget(self.dec_btn)
+label = QLabel("0")
+label.setStyleSheet("font-size: 20px;")
 
-        vbox=QVBoxLayout()
-        vbox.addWidget(self.label)
-        vbox.addLayout(hbox)
+inc_button = QPushButton("Increment")
+dec_button = QPushButton("Decrement")
 
-        self.setLayout(vbox)
+def increment():
+    global count
+    count += 1
+    label.setText(str(count))
 
-    def increment(self):
-        self.count+=1
-        self.label.setText(str(self.count))
-    def decrement(self):
-        self.count-=1
-        self.label.setText(str(self.count))
+def decrement():
+    global count
+    count -= 1
+    label.setText(str(count))
 
-app=QApplication(sys.argv)
-window=CounterApp()
+inc_button.clicked.connect(increment)
+dec_button.clicked.connect(decrement)
+
+layout = QVBoxLayout()
+layout.addWidget(label)
+layout.addWidget(inc_button)
+layout.addWidget(dec_button)
+
+window.setLayout(layout)
 window.show()
+
 sys.exit(app.exec())
